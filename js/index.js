beca496af63be1c2f25229b93aff262ba7da0ac8
Shea Fitzek <agg817@sbcglobal.net>
2017-09-13 18:33:17 -0500

    Updates

--- js/index.js
+++ js/index.js
@@ -3,17 +3,18 @@ const app = {
 		const checkBoxes = document.querySelectorAll(`.inbox input[type="checkbox"]`);
 
 		let lastChecked;
+		let lastUnchecked;
 
-		function handleCheck(e) {
-			let inBetween = false;
+		function handleChecked(e) {
+			let inBetweenChecked = false;
 
 			if (e.shiftKey && e.target.checked) {
 				checkBoxes.forEach((checkBox)=> {
 					if (checkBox === e.target || checkBox === lastChecked) {
-						inBetween = !inBetween;
+						inBetweenChecked = !inBetweenChecked;
 					}
 
-					if (inBetween) {
+					if (inBetweenChecked) {
 						checkBox.checked = true;
 					}
 				});
@@ -21,11 +22,39 @@ const app = {
 
 			if (e.target.checked) {
 				lastChecked = e.target;
+				lastUnchecked = null;
 			}
 		}
 
+		function handleUnhecked(e) {
+			let inBetweenUnchecked = false;
+
+			if (e.shiftKey && !e.target.checked) {
+				checkBoxes.forEach((checkBox)=> {
+					if (checkBox === e.target || checkBox === lastUnchecked) {
+						inBetweenUnchecked = !inBetweenUnchecked;
+					}
+
+					if (inBetweenUnchecked) {
+						checkBox.checked = false;
+					}
+				});
+			}
+
+			if (!e.target.checked) {
+				lastUnchecked = e.target;
+				lastChecked = null;
+			}
+		}
+
+		function checkHandler(e) {
+			e.target.checked
+				? handleChecked(e)
+				: handleUnhecked(e);
+		}
+
 		checkBoxes.forEach(()=> {
-			addEventListener(`click`, handleCheck);
+			addEventListener(`click`, checkHandler);
 		});
 	},
 
